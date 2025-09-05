5b8748a51390a90ad82da922e83fcf4fd9cd969d
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-05 09:33:59 +0100

    backed code commit by ENGR. IPAYE

--- database.py
+++ database.py
@@ -5,3 +5,11 @@ from sqlalchemy.orm import sessionmaker
 # SQLite Database url
 SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
 
+# ENGINE
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": "false"})
+
+# session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# base class for models
+Base = declarative_base()
