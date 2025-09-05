faa66c4873ba0cd7d2fe2cb371c894a0236e8c71
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-05 11:20:16 +0100

    backed code commit by ENGR. IPAYE

--- crud.py
+++ crud.py
@@ -10,8 +10,29 @@ def create_blog(db: Session, blog: schemas.BlogCreate):
     db.refresh(new_blog)
     return new_blog
 
+
 def get_blogs(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.Blog).offset(skip).limit(limit).all()
+
+
+def get_blog_by_id(db: Session, blog_id: int):
+    return db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+
+
+def update_blog(db: Session, blog_id: int, blog_update: schemas.BlogUpdate):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        return None
+
+    if blog_update.title is not None:
+        blog.title = blog_update.title
+    if blog_update.content is not None:
+        blog.content = blog_update.content
+
+    db.commit()
+    db.refresh(blog)
+    return blog
+
 
 def delete_blog(db: Session, blog_id: int):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import models, schemas, crud, database
+import models
+import schemas
+import crud
+import database
 
 # Create database tables
 models.Base.metadata.create_all(bind=database.engine)
@@ -28,6 +31,24 @@ def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db)):
 @app.get("/blogs/", response_model=list[schemas.BlogResponse])
 def read_blogs(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_blogs(db=db, skip=skip, limit=limit)
+
+
+# Get blog by id
+@app.get("/blogs/{blog_id}", response_model=schemas.BlogResponse)
+def read_blog(blog_id: int, db: Session = Depends(get_db)):
+    blog = crud.get_blog_by_id(db=db, blog_id=blog_id)
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    return blog
+
+
+# Update Blog by id
+@app.put("/blogs/{blog_id", response_model=schemas.BlogResponse)
+def update_blog(blog_id: int, blog_update: schemas.BlogUpdate, db: Session = Depends(get_db)):
+    updated_blog = crud.update_blog(db=db, blog_id=blog_id, blog_update=blog_update)
+    if not update_blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    return updated_blog
 
 
 # Delete a blog by ID (path param)
